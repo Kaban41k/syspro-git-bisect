7b769469377d42241caa6824725aa78352f6e906
Kaban41k <pig.anishenko@gmail.com>
2025-02-09 23:31:09 +0700

    Sigma legend

---
+++ main.py
@@ -0,0 +1,22 @@
+"""
+Did you ever hear the tragedy of Darth Plagueis the Rizzler?
+
+No
+
+I thought not
+It's not a story the Betas would tell you
+It's a Sigma legend
+Darth Rizzler was a Mog Lord of the Sigmas
+So powerful and so wise
+He could use his rizz to influence the Betas to show their gyat
+He became so powerful
+The only think he was afraid of was... losing his rizz
+which eventually, of course, he did
+
+Is it possible to learn this power?
+
+Not    from    a Beta
+"""
+
+print("You're a beta...")
+exit(0)
