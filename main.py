eb60d0ed3af9348201bf275672ca9787f43d1c3d
Kaban41k <pig.anishenko@gmail.com>
2025-02-09 23:35:16 +0700

    You have more rizz than the Chancellor

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 """
-You're on this concil
-But we do not grant you the rank of a rizzler
+I have more rizz than the Chancellor
+I can mog him
 
-What?
 
-How can you do this?
-This is outrageous
-It's unfair
-How can you be in this concil and not be a rizzler?
-
-Start mewing, young Skywalker
+I don't want to hear any more about Obi-Wan
+The Jedi mogged me
+Don't you mog me
 """
 
-print("You're on this concil, but still a beta...")
+print("You have more rizz than the Chancellor... But still a beta")
 exit(0)
