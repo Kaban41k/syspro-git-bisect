5324261f456abd385adf350b21c01549efc0fb93
Kaban41k <pig.anishenko@gmail.com>
2025-02-09 23:33:30 +0700

    Start mewing

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-print("Still a beta...")
+"""
+You're on this concil
+But we do not grant you the rank of a rizzler
+
+What?
+
+How can you do this?
+This is outrageous
+It's unfair
+How can you be in this concil and not be a rizzler?
+
+Start mewing, young Skywalker
+"""
+
+print("You're on this concil, but still a beta...")
 exit(0)
