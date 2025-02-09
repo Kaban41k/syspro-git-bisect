3db4b31bbf87540be5893842a7ad9812425c5a1d
Kaban41k <pig.anishenko@gmail.com>
2025-02-09 23:40:38 +0700

    Oh yeah, you're SO SIGMA

--- main.py
+++ main.py
@@ -1,24 +1,2 @@
-"""
-You're a Sigma male, aren't you?
-
-Are you capping, lil bro?
-
-I saw your jawline
-Only Sigmas can have such a mewing streak
-
-Perhaps I mogged a Sigma and fanum taxed him
-
-I don't think so
-No one can mog a Sigma
-
-You're a new gen, Anakin
-You're cooked
-
-
-I was once a looksmaxxer the same as your father
-He had the best jawline in the galaxy
-And he edged a lot
-"""
-
-print("You're so sigma")
+print("You're SO SIGMA")
 exit(1)
